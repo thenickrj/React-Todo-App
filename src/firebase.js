ce0c239a33934fea7d1429cf642b1e44cd33a061
Nikhil Singh <nikhilbangalor@gmail.com>
2021-04-26 09:38:08 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyD_f1Qgg7m5zV9HDMHFDSlO8BEw5qE0nDo",
-  authDomain: "todo-app-113a5.firebaseapp.com",
-  databaseURL: "https://todo-app-113a5.firebaseio.com",
-  projectId: "todo-app-113a5",
-  storageBucket: "todo-app-113a5.appspot.com",
-  messagingSenderId: "184438950936",
-  appId: "1:184438950936:web:ea97ff3993c1f7a271476d",
-  measurementId: "G-6WRPJTDKCW",
+  //Firebase config
 });
 
 const db = firebaseApp.firestore();
